089521c6fc71eb7b1d7be73103ebb71abe45e4b3
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-09-04 09:36:53 -0500

    first commit

---
+++ revision/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+//numero 5
+//int main()
+//{
+//    char a;
+//    cin >> a;
+//    int n = static_cast < int > (a);
+//    if (47 < n and n < 58){
+//        cout << " el caracter es un numero" << endl;
+//    }
+//    else {
+//        cout << "el caracter no es un numero" << endl;
+//    }
+//    return 0;
+//}
+// numero 6
+//int main ()
+//{
+//    char a;
+//    cout << "ingrese un caracter:" << endl;
+//    cin >> a;
+//    char d;
+//
+//    int n = static_cast < int > (a);
+//    if (96 < n and n < 123){
+//        cout << "su caracter es una letra minuscula" << endl;
+//        d = n - 32;
+//        char p = static_cast < char > (d);
+//        cout << p << endl;
+//    }
+//    if (64 < n and n < 91){
+//        cout << "su caracter es una letra mayuscula" << endl;
+//        d = n + 32;
+//        char p = static_cast < char > (d);
+//        cout << p << endl;
+//    }
+//
+//    return 0;
+//}
